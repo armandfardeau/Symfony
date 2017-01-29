671c3451558cd69f0414f3f336971018aab70e82
Armand Fardeau <fardeauarmand@gmail.com>
2017-01-30 00:06:05 +0100

    add get-user id block // TODO ask Yann about show session metadat  with profiler

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,8 +5,9 @@ namespace OC\PlatformBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class AdvertController extends Controller
@@ -18,10 +19,35 @@ class AdvertController extends Controller
         return new Response($content);
     }
 
-    public function viewAction($id)
+    public function viewAction($id, Request $request)
     {
-        $content = $this->get('templating')->render('OCPlatformBundle:Advert:view.html.twig', array('id'=>$id));
-        return new Response($content);
+
+        /*
+                 $tag = $request->query->get('tag');
+
+                $content = $this->get('templating')->render('OCPlatformBundle:Advert:view.html.twig', array(
+                    'id' => $id,
+                    'tag' => $tag,
+                ));
+                return new Response($content);
+        */
+
+        /*
+         * TODO
+         * voir avec Yann pour voir les request avec le profiler
+         * */
+
+
+        $session = $request->getSession();
+
+        // On récupère le contenu de la variable user_id
+        $userId = $session->get('user_id');
+
+        // On définit une nouvelle valeur pour cette variable user_id
+        $session->set('user_id', 91);
+
+        // On n'oublie pas de renvoyer une réponse
+        return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");
 
     }
 
