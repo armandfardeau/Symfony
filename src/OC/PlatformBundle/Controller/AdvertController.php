05da3d1547dc924a95dbda2758bded02318862b0
Armand Fardeau <fardeauarmand@gmail.com>
2017-01-29 03:07:18 +0100

    add proper view to method viewSlug

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -30,10 +30,8 @@ class AdvertController extends Controller
 
     public function viewSlugAction($slug, $year, $format)
     {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            slug '".$slug."', créée en ".$year." et au format ".$format."."
-        );
+        $content = $this->get('templating')->render('@OCPlatform/Advert/viewSlug.html.twig', array('slug'=>$slug, 'year'=>$year, 'format'=>$format));
+        return new Response($content);
     }
 }
 
