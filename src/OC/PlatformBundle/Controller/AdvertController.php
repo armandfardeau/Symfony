706c7ccb0e1cd5469cb7fad7050e8d71677f25e0
Armand Fardeau <fardeauarmand@gmail.com>
2017-02-02 15:05:26 +0100

    prepare to switch routing from yaml to annotation

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -1,78 +1,125 @@
 <?php
 
+// src/OC/PlatformBundle/Controller/AdvertController.php
 
 namespace OC\PlatformBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
-    {
-        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Armand'));
+    /**
+     * @Route(
+     *     "/{page}",
+     *     defaults={"page": "1"},
+     *     requirements={
+     *         "page": "\d*",
+     *     }
+     * )
+     */
 
-        return new Response($content);
+    public function indexAction($page)
+    {
+        if ($page < 1) {
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+        }
+
+        // Notre liste d'annonce en dur
+        $listAdverts = array(
+            array(
+                'title' => 'Recherche développpeur Symfony',
+                'id' => 1,
+                'author' => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date' => new \Datetime()),
+            array(
+                'title' => 'Mission de webmaster',
+                'id' => 2,
+                'author' => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date' => new \Datetime()),
+            array(
+                'title' => 'Offre de stage webdesigner',
+                'id' => 3,
+                'author' => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date' => new \Datetime())
+        );
+
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts,
+        ));
     }
 
-    public function viewAction($id, Request $request)
+    public function viewAction($id)
     {
+        $advert = array(
+            'title' => 'Recherche développpeur Symfony',
+            'id' => $id,
+            'author' => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date' => new \Datetime()
+        );
 
-        /*
-                 $tag = $request->query->get('tag');
-
-                $content = $this->get('templating')->render('OCPlatformBundle:Advert:view.html.twig', array(
-                    'id' => $id,
-                    'tag' => $tag,
-                ));
-                return new Response($content);
-        */
-
-        /*
-         * TODO
-         * voir avec Yann pour voir les request avec le profiler
-         * */
-
-
-        $session = $request->getSession();
-
-        // On récupère le contenu de la variable user_id
-        $userId = $session->get('user_id');
-
-        // On définit une nouvelle valeur pour cette variable user_id
-        $session->set('user_id', 91);
-
-        // On n'oublie pas de renvoyer une réponse
-        return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");
-
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
-    public function addAction($id)
+    public function addAction(Request $request)
     {
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
+            // Puis on redirige vers la page de visualisation de cettte annonce
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
     }
 
-    public function editAction($id)
+    public function editAction($id, Request $request)
     {
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
 
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+
+        $advert = array(
+            'title' => 'Recherche développpeur Symfony',
+            'id' => $id,
+            'author' => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date' => new \Datetime()
+        );
+
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
     public function deleteAction($id)
     {
-
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
     }
 
-    public function emailAction($id)
+    public function menuAction($limit)
     {
-        $content = $this->renderView('OCPlatformBundle:Advert:email.txt.twig', array(
-            'id' => $id,
-        ));
+        // On fixe en dur une liste ici, bien entendu par la suite on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
 
-        mail('moi@openclassrooms.com', 'Inscription OK', $content);
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
     }
 }
-
