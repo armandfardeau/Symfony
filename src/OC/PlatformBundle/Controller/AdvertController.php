d8cfb807c00ff61ed027ddc17a0d6a4a02001430
Armand Fardeau <fardeauarmand@gmail.com>
2017-02-03 11:43:24 +0100

    generate comment for advert controller

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,6 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Class AdvertController
+ * @package OC\PlatformBundle\Controller
+ */
 class AdvertController extends Controller
 {
     /**
@@ -20,8 +24,9 @@ class AdvertController extends Controller
      *         "page": "\d*",
      *     }
      * )
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-
     public function indexAction($page)
     {
         if ($page < 1) {
@@ -63,8 +68,9 @@ class AdvertController extends Controller
      *         "id": "\d+",
      *     }
      * )
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-
     public function viewAction($id)
     {
         $advert = array(
@@ -84,9 +90,9 @@ class AdvertController extends Controller
      * @Route("/add",
      *     name="oc_platform_add",
      * )
-     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-
     public function addAction(Request $request)
     {
         // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
@@ -109,9 +115,10 @@ class AdvertController extends Controller
      *         "id": "\d+",
      *     }
      * )
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-
-
     public function editAction($id, Request $request)
     {
         if ($request->isMethod('POST')) {
@@ -119,7 +126,6 @@ class AdvertController extends Controller
 
             return $this->redirectToRoute('oc_platform_view', array('id' => 5));
         }
-
         $advert = array(
             'title' => 'Recherche développpeur Symfony',
             'id' => $id,
@@ -127,7 +133,6 @@ class AdvertController extends Controller
             'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
             'date' => new \Datetime()
         );
-
         return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
             'advert' => $advert
         ));
@@ -141,13 +146,18 @@ class AdvertController extends Controller
      *         "id": "\d+",
      *     }
      * )
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-
     public function deleteAction($id)
     {
         return $this->render('OCPlatformBundle:Advert:delete.html.twig');
     }
 
+    /**
+     * @param $limit
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function menuAction($limit)
     {
         // On fixe en dur une liste ici, bien entendu par la suite on la récupérera depuis la BDD !
